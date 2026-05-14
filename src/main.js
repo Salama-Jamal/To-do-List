fd2ffb397dd1a683fffb325fc04b86cf4593db44
Jamal <mybusineess@gmail.com>
2026-05-15 00:03:13 +0100

    start create js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const newTaskInput = document.getElementById('new-task')
+const clearCompletedButton = document.getElementById('clear-completed')
+const taskList = document.createElement('ul')
+
+
+
+newTaskInput.addEventListener("click", function() {
+})
