b689b0b6f3196ac8e241be6ae31ccf966398a99c
Jamal <mybusineess@gmail.com>
2026-05-17 04:55:11 +0100

    push data to Firebase realtime database

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { getDatabase,
+                ref, 
+                push} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-database.js";
+
+
+
+const firebaseConfig = {
+databaseURL: "https://todo-list-ef861-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+const tasksRef = ref(database, 'tasks')
+
+
+
 const newTaskInput = document.getElementById('new-task')
-const clearCompletedButton = document.getElementById('clear-completed')
+const addCompletedButton = document.getElementById('add-completed')
 const taskList = document.createElement('ul')
 
 
 
-newTaskInput.addEventListener("click", function() {
+
+addCompletedButton.addEventListener("click", function() {
+    push(tasksRef, newTaskInput.value )
+    newTaskInput.value = ""
 })
